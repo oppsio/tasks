5240b7663354713e7b61744540f5e7319ea6a1f8
Ricardo Rossi <rrossi@endata.com>
2016-06-03 23:47:31 -0500

    small correction in deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 USER=ricardo-rossi
 cd $HOME
 git clone git@github.com:oppsio/releases.git
-mkdir ~/releases/$CIRCLE_BRANCH/
-cp ~/$CIRCLE_PROJECT_REPONAME/$CIRCLE_PROJECT_REPONAME ~/releases/$CIRCLE_BRANCH/
-cd ~/releases/
+mkdir -p ~/releases/$CIRCLE_BRANCH
+cp ~/$CIRCLE_PROJECT_REPONAME/$CIRCLE_PROJECT_REPONAME ~/releases/$CIRCLE_BRANCH
+cd ~/releases
 git add .
 git status
 COMMITMSG="Updating /$CIRCLE_BRANCH/$CIRCLE_PROJECT_REPONAME"
