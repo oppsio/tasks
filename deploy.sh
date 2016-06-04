3302ed41156ae05fe6281b434c7f50e122b599c2
Ricardo Rossi <rrossi@endata.com>
2016-06-03 23:56:58 -0500

    making script more verbose

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-USER=ricardo-rossi
+git config --global user.email "ricardo@endata.com"
+git config --global user.name "Ricardo Rossi"
 cd $HOME
 git clone git@github.com:oppsio/releases.git
 mkdir -p ~/releases/$CIRCLE_BRANCH
@@ -8,7 +9,7 @@ cp ~/$CIRCLE_PROJECT_REPONAME/$CIRCLE_PROJECT_REPONAME ~/releases/$CIRCLE_BRANCH
 cd ~/releases
 git add .
 git status
-# COMMITMSG="Updating /$CIRCLE_BRANCH/$CIRCLE_PROJECT_REPONAME"
-# echo $COMMITMSG
-git commit -v -m "new release"
+COMMITMSG="Updating /$CIRCLE_BRANCH/$CIRCLE_PROJECT_REPONAME"
+echo $COMMITMSG
+git commit -v -m "$COMMITMSG"
 git push -v --progress
